e66b4f9de6f43901d55106e5939607c58e9dbd24
Jinhyang <21500171@handong.edu>
2021-09-16 00:42:09 +0900

    Update: RxSwift - Transforming Operators

--- RxSwift/RxSwiftExample/RxSwiftPlayground.playground/Contents.swift
+++ RxSwift/RxSwiftExample/RxSwiftPlayground.playground/Contents.swift
@@ -3,37 +3,99 @@
 import UIKit
 import RxSwift
 
-var start = 0
-
-func getStartNumber() -> Int {
-    start += 1
-    return start
+public func example(of description: String,
+                    action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
 }
 
-let numbers = Observable<Int>.create { observer in
-    let start = getStartNumber()
-    observer.onNext(start)
-    observer.onNext(start+1)
-    observer.onNext(start+2)
-    observer.onCompleted()
-    return Disposables.create()
+// MARK: - Chatper 7: Transforming Operators
+
+example(of: "toArray") {
+    let disposeBag = DisposeBag()
+
+    Observable.of("A", "B", "C")
+        .toArray()
+        .subscribe(onSuccess: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
 }
 
-numbers.share().subscribe { element in
-    print("[1] element \(element)")
-} onCompleted: {
-    print("------------------")
+example(of: "map") {
+    let disposeBag = DisposeBag()
+
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
+    Observable<Int>.of(123, 4, 56)
+        .map({
+            formatter.string(for: $0) ?? ""
+        })
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
 }
 
-numbers.share().subscribe { element in
-    print("[2] element \(element)")
-} onCompleted: {
-    print("------------------")
+
+example(of: "enumerated and map") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .enumerated()
+        .map { index, integer in
+            index > 2 ? integer * 2 : integer
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
 }
 
-numbers.share().subscribe { element in
-    print("[3] element \(element)")
-} onCompleted: {
-    print("------------------")
+example(of: "compactMap") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("To", "be", nil, "or", "not", "to", "be", nil)
+        .compactMap({ $0 })
+        .toArray()
+        .map({ $0.joined(separator: " ") })
+        .subscribe(onSuccess: { print($0) })
+        .disposed(by: disposeBag)
 }
 
+
+struct Student {
+    let score: BehaviorSubject<Int>
+}
+
+example(of: "flatMap") {
+    let disposeBag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 90))
+
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMap {
+            $0.score
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
+    student
+        .map {
+            $0.score
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
+    student.onNext(laura) // 80
+    laura.score.onNext(85) // 85
+    student.onNext(charlotte) // 90
+}
