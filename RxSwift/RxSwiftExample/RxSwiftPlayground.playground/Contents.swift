cda4d0c7abece0a2d962d284f90cea3d0647783c
Jinhyang <21500171@handong.edu>
2021-09-27 23:55:31 +0900

    Update: RxSwift - Transforming Operators observing events

--- RxSwift/RxSwiftExample/RxSwiftPlayground.playground/Contents.swift
+++ RxSwift/RxSwiftExample/RxSwiftPlayground.playground/Contents.swift
@@ -99,3 +99,69 @@ example(of: "flatMap") {
     laura.score.onNext(85) // 85
     student.onNext(charlotte) // 90
 }
+
+example(of: "flatMapLatest") {
+    let disposeBag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 90))
+
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMapLatest {
+            $0.score
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
+    student.onNext(laura)
+    laura.score.onNext(85)
+    student.onNext(charlotte)
+    
+    laura.score.onNext(95)
+    charlotte.score.onNext(100)
+}
+
+
+example(of: "materialize and dematerialize") {
+    enum MyError: Error {
+        case anError
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 100))
+    
+    let student = BehaviorSubject(value: laura)
+    
+    let studentScore = student
+        .flatMapLatest {
+            $0.score.materialize()
+        }
+    
+    studentScore
+        .filter {
+            guard $0.error == nil else {
+                print($0.error!)
+                return false
+            }
+            
+            return true
+        }
+        .dematerialize()
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: disposeBag)
+    
+    laura.score.onNext(85)
+    laura.score.onError(MyError.anError)
+    laura.score.onNext(90)
+    
+    student.onNext(charlotte)
+}
+
