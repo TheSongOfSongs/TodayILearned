21d62002ffa82ce169b1de604bba34b6787569af
Jinhyang <21500171@handong.edu>
2021-12-02 21:58:44 +0900

    Feat: MemoListVC 뷰 바인딩

--- RxSwift/RxMemo/RxMemo/AppDelegate.swift
+++ RxSwift/RxMemo/RxMemo/AppDelegate.swift
@@ -11,10 +11,16 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let coordinator = SceneCoordinator(window: window!)
+        let storage = MemoryStorage()
+        let listViewModel = MemoListViewModel(title: "나의 메모",
+                                              sceneCoordinator: coordinator,
+                                              storage: storage)
+        let listScene = Scene.list(listViewModel)
+        coordinator.transition(to: listScene, using: .root, animated: false)
         return true
     }
 
--- RxSwift/RxMemo/RxMemo/ViewController/MemoListViewController.swift
+++ RxSwift/RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -6,8 +6,14 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
+    
+    @IBOutlet var listTableView: UITableView!
+    @IBOutlet var addButton: UIBarButtonItem!
     
     var viewModel: MemoListViewModel!
     
@@ -16,6 +22,14 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
     }
     
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
+        viewModel.memoList
+            .bind(to: listTableView.rx.items(cellIdentifier: "cell")) { row, memo, cell in
+                cell.textLabel?.text = memo.content
+            }
+            .disposed(by: rx.disposeBag)
     }
 }
